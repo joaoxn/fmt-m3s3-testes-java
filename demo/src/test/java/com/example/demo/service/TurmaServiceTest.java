e10bdf55a715f888de937750ac5cc1835f7f7b28
João Olivo <jvso@yahoo.com>
2024-11-07 21:37:46 -0300

    TurmaServiceTest: tests completed (ex. 3)

---
+++ demo/src/test/java/com/example/demo/service/TurmaServiceTest.java
@@ -0,0 +1,101 @@
+package com.example.demo.service;
+
+import com.example.demo.database.entities.Estudante;
+import com.example.demo.database.entities.Turma;
+import com.example.demo.database.entities.Turma;
+import com.example.demo.database.repositories.TurmaRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class TurmaServiceTest {
+
+    private final Long defaultId = 0L;
+
+    @Mock
+    private TurmaRepository turmaRepository;
+
+    @InjectMocks
+    private TurmaService service;
+
+    @Test
+    void testCadastrarTurma() {
+        String nome = "testNome";
+
+        Turma result = service.cadastrarTurma(nome);
+
+        assertEquals(nome, result.getNome());
+        verify(turmaRepository, times(1)).save(any(Turma.class));
+    }
+
+    @Test
+    void testListarTurmas() {
+        service.listarTurmas();
+        verify(turmaRepository, times(1)).findAll();
+    }
+
+    @Test
+    void testBuscarTurmaPorId() {
+        assertThrows(RuntimeException.class, () -> service.buscarTurmaPorId(defaultId));
+        verify(turmaRepository, times(1)).findById(defaultId);
+    }
+
+    @Test
+    void testAtualizarTurma() {
+        Turma turma = new Turma();
+        String novoNome = "testNome";
+
+        when(turmaRepository.findById(defaultId)).thenReturn(Optional.of(turma));
+        Turma result = service.atualizarTurma(defaultId, novoNome);
+
+        assertEquals(novoNome, result.getNome());
+        verify(turmaRepository, times(1)).findById(defaultId);
+    }
+
+    @Test
+    void testRemoverTurma() {
+        service.removerTurma(defaultId);
+        verify(turmaRepository, times(1)).deleteById(defaultId);
+    }
+
+    @Test
+    void testAdicionarEstudanteNaTurma() {
+        Estudante estudante = new Estudante();
+        Turma turma = new Turma();
+        estudante.setTurma(new ArrayList<>());
+
+        when(turmaRepository.findById(defaultId)).thenReturn(Optional.of(turma));
+
+        Estudante result = service.adicionarEstudanteNaTurma(defaultId, estudante);
+
+        assertEquals(estudante, result);
+        assertTrue(result.getTurma().contains(turma));
+        verify(turmaRepository, times(1)).findById(defaultId);
+    }
+
+    @Test
+    void testRemoverEstudanteNaTurma() {
+        Estudante estudante = new Estudante();
+        Turma turma = new Turma();
+        estudante.setTurma(new ArrayList<>(List.of(new Turma[]{turma})));
+
+        when(turmaRepository.findById(defaultId)).thenReturn(Optional.of(turma));
+
+        Estudante result = service.removerEstudanteDaTurma(defaultId, estudante);
+
+        assertEquals(estudante, result);
+        assertFalse(result.getTurma().contains(turma));
+        verify(turmaRepository, times(1)).findById(defaultId);
+    }
+}
